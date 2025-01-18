ca44698d11668f37cda09fbdcd63f8be39308447
81joker <nehad.al.timimi@gmail.com>
2025-01-18 14:19:46 +0100

    install  createStore + Tailwindcss

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,5 +1,6 @@
-import './assets/main.css'
-
+// import './assets/main.css'
+import './assets/index.css'
+import store from './store'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(store)
 
 app.mount('#app')
---
+++ backend/src/views/Login.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1 class="bg-red-500">Welcome to my Login!</h1>
+    <p>This is a simple template for Login .</p>
+  
+</main>
+</template>
