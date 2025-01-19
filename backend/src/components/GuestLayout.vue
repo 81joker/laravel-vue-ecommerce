e1313830543668ae7f40e40b87edf564e3ae8a90
Nehad <35563834+81joker@users.noreply.github.com>
2025-01-19 11:41:57 +0100

    create layout Auth + create login + Requestpass + Resetpass

---
+++ backend/src/components/GuestLayout.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+        <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            {{ title }}
+        </h2>
+      </div>
+
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+
+        <!-- <form class="space-y-6" action="#" method="POST"  @submit.prevent="emit('submit')"> -->
+        <slot></slot>
+        <!-- </form> -->
+
+        <p class="mt-10 text-center text-sm/6 text-gray-500">
+          Not a member?
+          {{ ' ' }}
+          <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+        </p>
+      </div>
+    </div>
+  </template>
+  <script setup>
+const {title} = defineProps({
+    title: String
+});
+
+// const emit = defineEmits(['submit'])
+
+  </script>
--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,29 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/Login.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/Login.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
-    }
-  ],
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: HomeView,
+        },
+        {
+            path: "/about",
+            name: "about",
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import("../views/AboutView.vue"),
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: LoginView,
+        },
+        {
+            path: "/request-password",
+            name: "requestPassword",
+            component: RequestPassword,
+        },
+        {
+            path: "/reset-password",
+            name: "resetPassword",
+            component: ResetPassword,
+        },
+    ],
+});
 
-export default router
+export default router;
