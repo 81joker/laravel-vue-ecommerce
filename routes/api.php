e3a40f579dbfec83f20a8cb28ebd727c87d390b5
81joker <nehad.al.timimi@gmail.com>
2025-01-21 08:09:58 +0100

    make middeare Admin && make route group api sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum' , 'admin')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+});
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
