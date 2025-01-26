6a2c5b105bff8d83b62cfae746e06becfdb921e9
81joker <nehad.al.timimi@gmail.com>
2025-01-26 11:05:50 +0100

    install packge Laravel Sluggable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model
 {
-    //
+    use HasSlug;
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
