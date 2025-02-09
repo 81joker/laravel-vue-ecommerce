3ef760f7d4a7e2742644098ced688c655097b7b3
81joker <nehad.al.timimi@gmail.com>
2025-02-09 10:10:06 +0100

    create controller products and display product on blade

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = Product::query()->orderBy('updated_at', 'desc')->paginate(4);
+        return view('product.index', ['products' => $products]);
+    }
+}
