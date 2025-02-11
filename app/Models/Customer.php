db377cffae34ef25b3f6613b04ce4794a8c97bde
81joker <nehad.al.timimi@gmail.com>
2025-02-11 16:51:06 +0100

    Setup Eloquent Model Relations from Customer to Addresses and to User

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Enums\AddressType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Customer extends Model
 {
@@ -12,4 +14,23 @@ class Customer extends Model
     protected $primaryKey = 'user_id';
 
     protected $fillable = ['first_name', 'last_name', 'phone', 'status',];
+
+    public function user():HasOne
+    {
+        return $this->hasOne(User::class ,'id' , 'user_id');
+    }
+    private function _getAddresses():HasOne
+    {
+        return $this->hasOne(CustomerAddress::class ,'customer_id' , 'user_id');
+    }
+
+    public function shippingAddress():HasOne
+    {
+        return $this->_getAddresses()->where('type', '=', AddressType::Shipping->value);
+    }
+
+    public function billingAddress(): HasOne
+    {
+        return $this->_getAddresses()->where('type', '=', AddressType::Billing->value);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Customer;
 
 class User extends Authenticatable implements MustVerifyEmail 
 {
@@ -47,5 +49,10 @@ class User extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function customer(): HasOne
+    {
+        return $this->hasOne(Customer::class);
     }
 }
