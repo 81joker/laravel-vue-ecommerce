c4e449e48859888f447c76cbfee9d85a02d3b63d
81joker <nehad.al.timimi@gmail.com>
2025-02-27 10:27:59 +0100

    create + update user on axios and laravel

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\UserController;
 
 
 
@@ -12,6 +13,7 @@ use App\Http\Controllers\Api\OrderController;
 Route::middleware(['auth:sanctum' ,'admin'])->group(function () {
     Route::get('/user', [AuthController::class, 'getUser']);
     Route::post('/logout', action: [AuthController::class, 'logout']);
+    Route::apiResource('users', UserController::class);
     Route::apiResource('/products', ProductController::class);
     Route::get('/orders', [OrderController::class, 'index']);
     Route::get('/orders', [OrderController::class, 'index']);
