f60dd04e619a53b820f240d2cb756cdae0bfe20b
Nehad <35563834+81joker@users.noreply.github.com>
2025-03-14 19:22:32 +0100

    make debug on CutomerMOdal State null

--- backend/src/views/Customers/CustomerModal.vue
+++ backend/src/views/Customers/CustomerModal.vue
@@ -64,7 +64,7 @@
 
                             <CustomInput type="select" :select-options="countries" v-model="customer.billingAddress.country_code" label="Country Code"/>
                             <!-- <CustomInput type="select" :select-options="stateOptions" v-model="customer.billingAddress.state" label="State"/> -->
-                            <CustomInput v-if="!billingCountry.states" v-model="customer.billingAddress.state" label="State"/>
+                            <CustomInput v-if="!billingCountry?.states" v-model="customer.billingAddress.state" label="State"/>
                             <CustomInput v-else type="select" :select-options="billingStateOptions" v-model="customer.billingAddress.state" label="State"/>
 
                         </div>
@@ -78,7 +78,7 @@
                             <CustomInput  v-model="customer.shippingAddress.zipcode" label="Zip"/>
                             <CustomInput type="select" :select-options="countries" v-model="customer.shippingAddress.country_code" label="Country Code"/>
 
-                            <CustomInput v-if="!shippingCountry.states" v-model="customer.shippingAddress.state" label="State"/>
+                            <CustomInput v-if="!shippingCountry?.states" v-model="customer.shippingAddress.state" label="State"/>
                             <CustomInput v-else type="select" :select-options="shippingStateOptions" v-model="customer.shippingAddress.state" label="State"/>
 
                             </div>
