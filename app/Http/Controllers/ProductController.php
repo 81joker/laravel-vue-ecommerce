ff4ee1eabaa8099f41abbcbcf824ce16a4f434c6
81joker <nehad.al.timimi@gmail.com>
2025-03-19 10:46:34 +0100

    make published product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,8 +8,15 @@ use App\Models\Product;
 class ProductController extends Controller
 {
     public function index(){
-        $products = Product::query()->orderBy('updated_at', 'desc')->paginate(8);
-        return view('product.index', ['products' => $products]);
+        // $products = Product::query()->orderBy('updated_at', 'desc')->paginate(8);
+        // return view('product.index', ['products' => $products]);
+        $products = Product::query()
+            ->where('published', '=', 1)
+            ->orderBy('updated_at', 'desc')
+            ->paginate(5);
+        return view('product.index', [
+            'products' => $products
+        ]);
     }
 
     public function view(Product $product){
---
+++ backend/src/views/Report/Report.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-semibold">Report</h1>
+    <div class="mt-4">
+      <p>
+        This page is under construction. Please check back later.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Report",
+};
+</script>
