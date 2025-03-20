77e99537caf455ff873e90089e0c0a11d460db11
Nehad <35563834+81joker@users.noreply.github.com>
2025-03-20 09:00:40 +0100

    creatre controller Report + Traits Report

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    public function orders(){
+
+    }
+    public function customers(){
+
+    }
+}
